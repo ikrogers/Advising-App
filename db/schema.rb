6377c001351de754f3c49fd0a1f7d30e3d17cf1a
ikrogers <ikrogers92@gmail.com>
2014-03-25 16:38:31 -0400

    Added choices and message columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140322235010) do
+ActiveRecord::Schema.define(version: 20140325203732) do
 
   create_table "courselists", force: true do |t|
     t.string "name"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20140322235010) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "studentid"
+    t.integer  "choices"
+    t.string   "message"
   end
 
   create_table "users", force: true do |t|
