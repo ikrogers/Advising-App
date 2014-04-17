f2084a9e14e2c1b980eb570ef2259b8ff46b360a
ikrogers <ikrogers92@gmail.com>
2014-04-16 23:37:00 -0400

    Overhauled front page added loads of css classes to play with

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(:default, Rails.env)
 module Advising
   class Application < Rails::Application
     config.encoding = "utf-8"
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
