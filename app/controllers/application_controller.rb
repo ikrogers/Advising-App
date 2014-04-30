bd7ba4cf13e092a206ad0a5c5f668660e018831b
unknown <John@.(none)>
2014-04-29 21:42:25 -0400

    message modal looks and works as intended(lines are appearing with messages, haven't figured out how to correct

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   before_filter :store_path
   protect_from_forgery with: :exception
-  
-
+  before_action :authorize
   def store_path
     # Set "last_path" to the "current_path" set by the previous request
     session[:last_path] = session[:current_path] || root_path
     # And then update the current_path to the uri for the current request
     session[:current_path] = request.url
+  end
+
+  protected
+
+  def authorize
+    unless User.find_by(id: session[:user_id])
+      redirect_to login_url
+    end
   end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -34,7 +34,7 @@ class MessagesController < ApplicationController
     if @currentuser.classification == 'Advisor'
     respond_to do |format|
       if @message.save
-        format.html { redirect_to advisor_url, notice: 'Message was successfully created.' }
+        format.html { redirect_to advisor_url }
         format.json { render action: 'show', status: :created, location: @message }
       else
         format.html { render action: 'new' }
@@ -44,7 +44,7 @@ class MessagesController < ApplicationController
     elsif @currentuser.classification == 'Student'
       respond_to do |format|
       if @message.save
-        format.html { redirect_to student_url, notice: 'Message was successfully created.' }
+        format.html { redirect_to student_url, confirm: 'Message was successfully created.' }
         format.json { render action: 'show', status: :created, location: @message }
       else
         format.html { render action: 'new' }
