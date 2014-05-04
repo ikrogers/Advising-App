6c88d44feea8e1f0bf31e92337489309c9693228
wccassady <wccassady@valdosta.edu>
2014-05-04 03:31:10 -0400

    Admin can register students, essentially solidifying the course registration and readying system for next semester

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,7 +6,16 @@ class AdminController < ApplicationController
   def regstudent
     @courses = Course.where('choice is NOT NULL')
     @courses.each do |course|
-      course.update_attributes({ :studentid => course.choice, :choice => nil})
+      @user = User.find_by_id(course.choice)
+      if @user.flag == 'advised'
+        course.update_attributes({ :studentid => course.choice, :choice => nil})
+      else
+        course.delete
+      end
+    end
+    @users = User.where('flag != ?', 'false')
+    @users.each do |user|
+        user.update_attributes({ :flag => 'false' })
     end
     respond_to do |format|
       format.html { redirect_to admin_path, notice: 'Students registered!' }
